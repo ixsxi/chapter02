2c64bde3241490958319de4f0cb9ebd33dfd5e59
user <user@DESKTOP-PMC72TK>
2021-06-03 02:13:23 +0900

    song 연습문제

---
+++ src/com/javaex/ex04/Song.java
@@ -0,0 +1,47 @@
+package com.javaex.ex04;
+
+public class Song {
+
+	//필드
+	
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private int year;
+	private String track;
+	
+	//생성자
+	
+	//메소드 (게터,세터)
+	
+	// **************세터 ************
+	public void setTitle(String title){
+			this.title = title;
+	}
+	
+	public void setArtist(String artist) {
+			this.artist =artist;
+	}
+	public void setAlbum(String album) {
+			this.album =album;
+	}
+	public void setComposer(String composer) {
+			this.composer =composer;
+	}
+	public void setYear(int year) {
+			this.year =year;
+	}
+	public void setTrack(String track) {
+			this.track =track;
+	}
+	
+	//************게터*************** //게터는 필요한가 .. ?
+	
+	
+	public void showInfo(){
+		System.out.println(artist+ "," +title+ "(" +album+ "," +year+ "," +track + " track," + composer+"작곡"+")");
+	}
+	
+	
+}
---
+++ src/com/javaex/ex04/SongApp.java
@@ -0,0 +1,40 @@
+package com.javaex.ex04;
+
+public class SongApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Song iu= new Song();
+		Song bigbang = new Song();
+		Song busker = new Song();
+		//**************아이유 ***********8
+		iu.setTitle("좋은날");
+		iu.setArtist("아이유");
+		iu.setAlbum("real");
+		iu.setComposer("이민수");
+		iu.setYear(2010);
+		iu.setTrack("3번");
+		
+		//************빅뱅****************
+		bigbang.setTitle("거짓말");
+		bigbang.setArtist("BIGBANG");
+		bigbang.setAlbum("Always");
+		bigbang.setComposer("G-DRAGON");
+		bigbang.setYear(2007);
+		bigbang.setTrack("2번");
+		
+		//*************버스커*************8
+		busker.setTitle("벚꽃엔딩");
+		busker.setArtist("버스커버스커");
+		busker.setAlbum("버스커버스커1집");
+		busker.setComposer("장범준");
+		busker.setYear(2012);
+		busker.setTrack("4번");
+		
+		iu.showInfo();
+		bigbang.showInfo();
+		busker.showInfo();
+	}
+
+}
